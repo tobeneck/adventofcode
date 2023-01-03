115bb1308bb2805119224a211a13662cfc46fb63
tobeneck <tobias.benecke@ovgu.de>
2023-01-03 20:11:53 +0100

    Quick python results for day 1.

---
+++ code/day01.py
@@ -0,0 +1,21 @@
+calories_file = open("day01.txt")
+
+calorie_data = calories_file.readlines()
+
+curr_elve = 0
+elves = []
+
+for line in calorie_data:
+    curr_data = line.replace("\n", "")
+    if curr_data == "":
+        elves.append(curr_elve)
+        curr_elve = 0
+    else:
+        curr_elve += int(curr_data)
+
+print("The elf carrying the highest number of calories is carrying "+str( max(elves) )+" calories.")
+print("The three elves carrying the highest number of calories are in total carrying "+str( sum(sorted(elves, reverse=True)[:3]) )+" calories.")
+
+
+
+
